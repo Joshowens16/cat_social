5b85ba488ab12dd71b613c6f2f04c61c5a15eccd
Joshowens16 <joshowens16@gmail.com>
2023-03-18 12:11:21 -0700

    adding prisma instead of sequelize, going to be doing auth on my own

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,7 +1,4 @@
-import authRouter from "./auth";
 import express from "express";
 const router = express.Router();
-
-router.use("/auth", authRouter);
 
 export default router;
--- server/db/index.ts
+++
@@ -1,9 +0,0 @@
-import db from "./db";
-import User from "./User";
-
-const data = {
-  User,
-  db,
-};
-
-export default data;
