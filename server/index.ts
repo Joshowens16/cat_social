8eff4ee656bb3b9d5cd61ef2d35ab3b42350909d
Joshowens16 <joshowens16@gmail.com>
2023-03-23 09:57:55 -0700

    ts warning

--- server/index.ts
+++ server/index.ts
@@ -3,8 +3,10 @@ import app from "./app";
 const init = async () => {
   try {
     const port = process.env.PORT || 3000;
+    // tslint:disable-next-line: no-console
     app.listen(port, () => console.log(`listening on port ${port}`));
   } catch (ex) {
+    // tslint:disable-next-line: no-console
     console.log(ex);
   }
 };
