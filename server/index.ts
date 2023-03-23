8f07f67b07aef9436163c84dbf63042be9862527
Joshowens16 <joshowens16@gmail.com>
2023-03-23 10:07:19 -0700

    tslint rules

--- server/index.ts
+++ server/index.ts
@@ -3,10 +3,8 @@ import app from "./app";
 const init = async () => {
   try {
     const port = process.env.PORT || 3000;
-    // tslint:disable-next-line: no-console
     app.listen(port, () => console.log(`listening on port ${port}`));
   } catch (ex) {
-    // tslint:disable-next-line: no-console
     console.log(ex);
   }
 };
