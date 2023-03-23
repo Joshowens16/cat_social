11c1285f5956287c72b7ad68e0240fe6c66ecc8f
Joshowens16 <joshowens16@gmail.com>
2023-03-23 09:50:56 -0700

    JWT, brcrypt working

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import userRouter from "./user";
 const router = express.Router();
+import auth from "./auth.routes";
 
 router.use("/users", userRouter);
-
+router.use("/auth", auth);
 export default router;
