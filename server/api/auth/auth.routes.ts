ba9ddc5e24452241b9bb498c46965c666b91a37a
Joshowens16 <joshowens16@gmail.com>
2023-03-24 07:46:11 -0700

    getting refresh tokens and revoking now working

--- server/api/auth/auth.routes.ts
+++ server/api/auth/auth.routes.ts
@@ -8,7 +8,7 @@ import {
   deleteRefreshToken,
   revokeTokens,
 } from "./auth.services";
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import hashToken from "../../utils/hashToken";
 import {
   findUserByUsername,
@@ -91,67 +91,70 @@ router.post("/login", async (req, res, next) => {
   }
 });
 
-// router.post("/refreshToken", async (req, res, next) => {
-//   try {
-//     const { refreshToken } = req.body;
-//     if (!refreshToken) {
-//       res.status(400);
-//       throw new Error("Missing refresh token.");
-//     }
-//     if (!JWT_REFRESH) {
-//       throw new Error("No valid JWT refresh token");
-//     }
-//     const payload = jwt.verify(refreshToken, JWT_REFRESH);
-//     console.log(payload);
-//     const savedRefreshToken = await findRefreshTokenById(payload);
+router.post("/refreshToken", async (req, res, next) => {
+  try {
+    const { refreshToken } = req.body;
+    if (!refreshToken) {
+      res.status(400);
+      throw new Error("Missing refresh token.");
+    }
+    if (!JWT_REFRESH) {
+      throw new Error("No valid JWT refresh token");
+    }
 
-//     if (!savedRefreshToken || savedRefreshToken.revoked === true) {
-//       res.status(401);
-//       throw new Error("Unauthorized");
-//     }
+    const payload = jwt.verify(refreshToken, JWT_REFRESH) as JwtPayload;
+    if (!payload.jti) {
+      throw new Error("Invalid payload ID");
+    }
+    const savedRefreshToken = await findRefreshTokenById(payload.jti);
 
-//     const hashedToken = hashToken(refreshToken);
-//     if (hashedToken !== savedRefreshToken.hashedToken) {
-//       res.status(401);
-//       throw new Error("Unauthorized");
-//     }
+    if (!savedRefreshToken || savedRefreshToken.revoked === true) {
+      res.status(401);
+      throw new Error("Unauthorized");
+    }
 
-//     const user = await findUserById(payload.userId);
-//     if (!user) {
-//       res.status(401);
-//       throw new Error("Unauthorized");
-//     }
+    const hashedToken = hashToken(refreshToken);
+    if (hashedToken !== savedRefreshToken.hashedToken) {
+      res.status(401);
+      throw new Error("Unauthorized");
+    }
 
-//     await deleteRefreshToken(savedRefreshToken.id);
-//     const jti = uuidv4();
-//     const { accessToken, refreshToken: newRefreshToken } = generateTokens(
-//       user,
-//       jti
-//     );
-//     await addRefreshTokenToWhitelist({
-//       jti,
-//       refreshToken: newRefreshToken,
-//       userId: user.id,
-//     });
+    const user = await findUserById(payload.userId);
+    if (!user) {
+      res.status(401);
+      throw new Error("Unauthorized");
+    }
 
-//     res.json({
-//       accessToken,
-//       refreshToken: newRefreshToken,
-//     });
-//   } catch (err) {
-//     next(err);
-//   }
-// });
+    await deleteRefreshToken(savedRefreshToken.id);
+    const jti = uuidv4();
+    const { accessToken, refreshToken: newRefreshToken } = generateTokens(
+      user,
+      jti
+    );
+    await addRefreshTokenToWhitelist({
+      jti,
+      refreshToken: newRefreshToken,
+      userId: user.id,
+    });
 
-// // This endpoint is only for demo purpose.
-// // Move this logic where you need to revoke the tokens( for ex, on password reset)
-// router.post("/revokeRefreshTokens", async (req, res, next) => {
-//   try {
-//     const { userId } = req.body;
-//     await revokeTokens(userId);
-//     res.json({ message: `Tokens revoked for user with id #${userId}` });
-//   } catch (err) {
-//     next(err);
-//   }
-// });
+    res.json({
+      accessToken,
+      refreshToken: newRefreshToken,
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// This endpoint is only for demo purpose.
+// Move this logic where you need to revoke the tokens( for ex, on password reset)
+router.post("/revokeRefreshTokens", async (req, res, next) => {
+  try {
+    const { userId } = req.body;
+    await revokeTokens(userId);
+    res.json({ message: `Tokens revoked for user with id #${userId}` });
+  } catch (err) {
+    next(err);
+  }
+});
 export default router;
