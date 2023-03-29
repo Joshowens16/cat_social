3825da46d90247af05680d8af1b3614e232dc28d
Joshowens16 <joshowens16@gmail.com>
2023-03-28 20:27:33 -0700

    icons and names in sidebar

--- src/components/sidebar/SideBar.tsx
+++ src/components/sidebar/SideBar.tsx
@@ -1,10 +1,41 @@
 import React from "react";
 import "./sidebar.css";
+import { AiFillHome, AiOutlineSearch, AiOutlineMessage } from "react-icons/ai";
+import { CgProfile } from "react-icons/cg";
+
+const contents = [
+  {
+    icon: <AiFillHome />,
+    name: "Home",
+  },
+  {
+    icon: <AiOutlineSearch />,
+    name: "Search",
+  },
+  {
+    icon: <AiOutlineMessage />,
+    name: "Messages",
+  },
+  {
+    icon: <CgProfile />,
+    name: "Profile",
+  },
+];
 const SideBar = () => {
   return (
     <div className="sideBarContainer">
       <div className="sideBarContents">
         <h1>Cat Social</h1>
+        <div className="contents">
+          {contents.map((item) => {
+            return (
+              <div className="contentItem" key={item.name}>
+                {item.icon}
+                {item.name}
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
