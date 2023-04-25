6f36ce2ef36ce2816a0632a3b651601587fa5d0e
Joshowens16 <joshowens16@gmail.com>
2023-04-25 13:15:09 -0700

    only jpegs accepted for now

--- src/components/UploadPost.tsx
+++ src/components/UploadPost.tsx
@@ -7,24 +7,30 @@ import { RootState } from "../store/index";
 const UploadPost = () => {
   const [post, setPost] = useState(null);
   const username = useSelector((state: RootState) => state.user.username);
+  const userID = useSelector((state: RootState) => state.user.id);
+
   // console.log(userId);
   const handlePostChange = (e: ChangeEvent<HTMLInputElement>) => {
     if (!e.target.files) return console.log("File not found");
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const imageFile = e.target.files[0] as any;
+    if (imageFile.type !== "image/jpeg") {
+      alert("Photos must be in JPEG format");
+      return;
+    }
     // && (e.target.files[0] as any)
     setPost(imageFile);
   };
   const handleImageUpload = () => {
     if (!post) return;
-    const imageRef = ref(storage, `images/${v4()}`);
+    const imageRef = ref(storage, `${userID}/${v4()}`);
     uploadBytes(imageRef, post).then(() => {
       alert("Image uploaded");
     });
   };
   return (
     <div>
-      <input type="file" onChange={handlePostChange} />
+      <input type="file" accept="image/jpeg" onChange={handlePostChange} />
       <button onClick={handleImageUpload}>Upload Post</button>
     </div>
   );
